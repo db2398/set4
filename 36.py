41677290acc172c6729dc00478b68e98f4cf56eb
db2398 <51949608+db2398@users.noreply.github.com>
2019-07-03 11:43:34 +0530

    Update and rename 36.py to 36new.py

--- 36.py
+++
@@ -1,8 +0,0 @@
-t=input()
-k=0
-for i in range(len(t)):
- if(t[i].isdigit() or t[i].isalpha() or t[i]==(" ")):
-    continue
-  else:
-    f+=1
- print(k)
---
+++ 36new.py
@@ -0,0 +1,8 @@
+g=input()
+z=0
+for i in range(len(g)):
+ if(g[i].isdigit() or g[i].isalpha() or g[i]==(" ")):
+    continue
+  else:
+    f+=1
+ print(z)
